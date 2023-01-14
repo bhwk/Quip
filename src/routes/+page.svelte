b0057c8da230f2bfd3ad2d5f46546c59b94ec116
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-14 16:56:53 +0800

    Add borders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<div class="flex flex-1 flex-col h-screen">
+<div class="flex flex-1 flex-col">
 	<logo class="self-center">LOGO</logo>
 	<div id="lobbyContainer" class="flex flex-1 justify-around">
-		<div>
+		<div class="border-2 border-black">
 			<h1>Tutorial header</h1>
 		</div>
-		<div class="flex flex-col text-center">
+		<div class="flex flex-col border-2 border-black text-center">
 			<h1>Lobby header</h1>
 			<input type="text" placeholder="text" />
 			<span>LOBBY CODE</span>
