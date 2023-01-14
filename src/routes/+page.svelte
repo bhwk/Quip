c1e435677282ab6249cc60579c2a11015bada4b8
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-14 16:38:57 +0800

    Update styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <div class="flex flex-1 flex-col h-screen">
     <logo class="self-center">LOGO</logo>
-    <div id='lobbyContainer' class="flex flex-1 justify-around">
-        <div>
-            <h1>Tutorial header </h1>
+    <div id='lobbyContainer' class="flex flex-1 justify-center">
+        <div class="border-4 border-black w-52">
+            <h1>How to play </h1>
+            <ul>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+            </ul>
         </div>
-        <div class="flex flex-col text-center">
+        <div class="flex flex-col flex-1 justify-center text-center border-black border-4 p-4 box-border">
             <h1>Lobby header</h1>
-            <input type="text" placeholder="text">
-            <span>LOBBY CODE</span>
-            <div class="flex justify-between">
+            <input class="max-w-fit mx-auto" type="text" placeholder="Name">
+            <l>LOBBY CODE</l>
+            <div class="flex justify-around">
                 <button>CREATE</button>
                 <button>JOIN</button>
             </div>
