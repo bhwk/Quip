0c71dc0da54a1beab38a93f5587f240ff29c56ac
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-14 16:42:11 +0800

    Accept current changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <div class="flex flex-1 flex-col h-screen">
-<<<<<<< HEAD
 	<logo class="self-center">LOGO</logo>
 	<div id="lobbyContainer" class="flex flex-1 justify-around">
 		<div>
@@ -15,26 +14,4 @@
 			</div>
 		</div>
 	</div>
-=======
-    <logo class="self-center">LOGO</logo>
-    <div id='lobbyContainer' class="flex flex-1 justify-center">
-        <div class="border-4 border-black w-52">
-            <h1>How to play </h1>
-            <ul>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-                <li>Lorem ipsum</li>
-            </ul>
-        </div>
-        <div class="flex flex-col flex-1 justify-center text-center border-black border-4 p-4 box-border">
-            <h1>Lobby header</h1>
-            <input class="max-w-fit mx-auto" type="text" placeholder="Name">
-            <l>LOBBY CODE</l>
-            <div class="flex justify-around">
-                <button>CREATE</button>
-                <button>JOIN</button>
-            </div>
-        </div>
-    </div>
->>>>>>> c1e435677282ab6249cc60579c2a11015bada4b8
 </div>
