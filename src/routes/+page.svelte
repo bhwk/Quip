28defea153cb0164c9020281b066efb11e51f130
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-14 15:39:35 +0800

    Add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-1 flex-col h-screen">
+    <logo class="self-center">LOGO</logo>
+    <div id='lobbyContainer' class="grid grid-cols-2">
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore illo nulla corporis, quas, ipsam qui laudantium explicabo labore molestias, eos fuga sequi tenetur! Perferendis ullam consequatur velit, maiores eos voluptatibus.</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, vitae! Molestias neque reprehenderit quidem odio, voluptate tempore. Obcaecati ipsa minus natus sapiente dolor ratione non nisi assumenda harum. Eius, recusandae!</div>
+    </div>
+</div>
