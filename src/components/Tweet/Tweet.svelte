ea3181ae74ebc079212c4897d0ea6760438399f8
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-15 01:14:03 +0800

    Update tweet styling

--- src/components/Tweet/Tweet.svelte
+++ src/components/Tweet/Tweet.svelte
@@ -3,9 +3,11 @@
 	let content;
 </script>
 
-<div class="self-center flex grow-0 flex-col max-w-md bg-black rounded-3xl text-white p-4 box-border border border-gray-600">
+<div class="self-center flex gap-2 grow-0 max-w-md bg-black rounded-3xl text-white p-4 box-border border border-gray-600">
 	<div class="flex gap-2">
 		<div class="bg-white w-12 h-12 rounded-full"></div>
+	</div>
+	<div class="flex flex-col gap-2">
 		<div class="flex flex-col">
 			<div class="font-bold">
 				{username}
@@ -14,10 +16,10 @@
 				@{username}
 			</div>
 		</div>
-	</div>
-	<div class="text-sm">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, blanditiis? Sed quod
-		reprehenderit quis cum, reiciendis a odit praesentium aspernatur quisquam earum aut
-		explicabo, ratione esse laboriosam fugit molestias adipisci.
+		<div class="text-sm">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, blanditiis? Sed quod
+			reprehenderit quis cum, reiciendis a odit praesentium aspernatur quisquam earum aut
+			explicabo, ratione esse laboriosam fugit molestias adipisci.
+		</div>
 	</div>
 </div>
