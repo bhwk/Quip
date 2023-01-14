132c7614d7b1f3ee9e0efea9b66d90aa327e9e35
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-15 03:02:19 +0800

    Update styling on winner screen

--- src/components/Winner/Winner.svelte
+++ src/components/Winner/Winner.svelte
@@ -1,26 +1,28 @@
 <script>
-    let players = ['first','second','third'];
+    let players;
 </script>
 
-<div class="flex flex-col gap-10">
-    {#each players as player, i}
-        {#if i === 0}
-            <div class="flex flex-col gap-2 items-center">
-                <div class="bg-black rounded-full w-16 h-16"></div>
-                <div>🥇</div>
-                <div class="text-6xl">{player}</div>
-            </div>
-        {:else}
-            <div class="flex flex-col gap-2 items-center">
-                <div class="bg-black rounded-full w-10 h-10"></div>
-                {#if i ===1}
-                <div>🥈</div>
-                {:else}
-                <div>🥉</div>
+{#if players}
+<div class="flex flex-col gap-10 p-4">
+        <div class="flex flex-col gap-2 max-w-md self-center w-full items-center py-8 rounded-3xl bg-white shadow-xl border-t">
+            <div class="bg-black rounded-full w-28 h-28"></div>
+            <div class="text-7xl">🥇</div>
+            <div class="text-6xl">{players[0]}</div>
+        </div>
+        <div class="flex flex-col items-center md:flex-row justify-center gap-20">
+            {#each players as player, i}
+                {#if i>0} 
+                    <div class="flex flex-col grow-0 max-w-md gap-2 items-center px-20 py-8 rounded-3xl bg-white shadow-xl border-t">
+                        <div class="bg-black rounded-full w-16 h-16"></div>
+                        {#if i ===1}
+                        <div class="text-4xl">🥈</div>
+                        {:else}
+                        <div class="text-4xl">🥉</div>
+                        {/if}
+                        <div class="text-base">{player}</div>
+                    </div>
                 {/if}
-                <div class="text-base">{player}</div>
-            </div>
-        {/if}
-    {/each}
-
+            {/each}
+        </div>
 </div>
+{/if}
