efee7bd8dc935e74caeb97900af06cbc382d3ada
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-14 17:06:52 +0800

    Add lobby page placeholder

---
+++ src/routes/lobby/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col items-center flex-1 p-4 box-border gap-4">
+    <div class="text-center flex flex-col gap-2">
+        <span id="lobbyCode" class="border-2 border-black">LOBBY CODE</span>
+        <div id='logo' class="mx-auto border-2 border-black">LOGO</div>
+    </div>
+    <div class="flex flex-col gap-2">
+        <ul id='playerList' class="border-2 border-black p-4 w-auto">
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+            <li>Lorem ipsum</li>
+        </ul>
+        <div>
+            <button>START</button>
+            <button>LEAVE</button>
+        </div>
+    </div>
+</div>
