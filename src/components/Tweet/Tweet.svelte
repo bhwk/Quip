3e237f1ac53fa2eb26f01a9880a137e8b54c2f32
bhwk <120708160+bhwk@users.noreply.github.com>
2023-01-15 00:40:07 +0800

    Tweet card

--- src/components/Tweet/Tweet.svelte
+++ src/components/Tweet/Tweet.svelte
@@ -3,12 +3,12 @@
 	let content;
 </script>
 
-<div class="flex flex-col bg-black rounded-full">
+<div class="self-center flex grow-0 flex-col max-w-md bg-black rounded-3xl text-white p-4 border border-gray-600">
 	<div class="flex flex-col">
 		<div class="font-bold">
 			{username}
 		</div>
-		<div class="text-slate-600 text-sm">
+		<div class="text-neutral-400 text-sm">
 			@{username}
 		</div>
 	</div>
